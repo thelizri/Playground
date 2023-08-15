ac510f7f1d79317965b7c4236ca818fde0278b37
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-08-15 02:57:20 +0200

    Update

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 import uuid
 
@@ -16,12 +17,23 @@ def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
 
 @app.post("/store")
 def post_store():
     store_data = request.get_json()
+
+    # Validating required arguments
+    if "name" not in store_data:
+        abort(400, "Bad request. Make sure 'name' is included in the JSON payload.")
+
+    # Checking if store already exist
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, "Store already exist.")
+
+    # Adding new store
     store_id = uuid.uuid4().hex
     new_store = {**store_data, "id": store_id}
     stores[store_id] = new_store
@@ -39,18 +51,46 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found")
 
 
 @app.post("/item")
 def post_item_to_store():
     item_data = request.get_json()
-    try:
-        if item_data["store_id"] not in stores:
-            return {"message": "Store not found"}, 404
-    except KeyError:
-        return {"message": "Item needs store id"}, 400
+
+    # Validating required arguments
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            "Bad request. Make sure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+
+    # Checking if item already exist
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(
+                400,
+                "Item already exists",
+            )
+
+    # Adding new item
     item_id = uuid.uuid4().hex
     new_item = {**item_data, "id": item_id}
     items[item_id] = new_item
     return new_item, 201
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except KeyError:
+        abort(404, message="Item not found")
--- Flask/db.py
+++ Flask/db.py
@@ -13,12 +13,12 @@ stores = {
 
 # Items dictionary
 items = {
-    "1": {"name": "Chair", "price": 50.00, "id": 1},
-    "2": {"name": "Sofa", "price": 300.00, "id": 2},
-    "3": {"name": "Laptop", "price": 1000.00, "id": 3},
-    "4": {"name": "TV", "price": 500.00, "id": 4},
-    "5": {"name": "Speaker", "price": 100.00, "id": 5},
-    "6": {"name": "Apple", "price": 0.50, "id": 6},
-    "7": {"name": "Milk", "price": 2.00, "id": 7},
-    "8": {"name": "Bread", "price": 1.50, "id": 8},
+    "1": {"name": "Chair", "price": 50.00, "id": 1, "store_id": 1},
+    "2": {"name": "Sofa", "price": 300.00, "id": 2, "store_id": 1},
+    "3": {"name": "Laptop", "price": 1000.00, "id": 3, "store_id": 1},
+    "4": {"name": "TV", "price": 500.00, "id": 4, "store_id": 1},
+    "5": {"name": "Speaker", "price": 100.00, "id": 5, "store_id": 1},
+    "6": {"name": "Apple", "price": 0.50, "id": 6, "store_id": 1},
+    "7": {"name": "Milk", "price": 2.00, "id": 7, "store_id": 1},
+    "8": {"name": "Bread", "price": 1.50, "id": 8, "store_id": 1},
 }
