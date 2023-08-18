1c6cafa3e777a47047222c95b25003d8a218c66b
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-08-18 19:47:48 +0200

    Update

--- Django/monthly_challenges/challenges/views.py
+++ Django/monthly_challenges/challenges/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+months = {
+    "january": "It's cold outside. Wear a jacket.",
+    "february": "Go to the gym. Work off that holiday weight.",
+    "march": "Almost spring. Start preparing your beach body.",
+    "april": "April showers bring May flowers. Don't forget your umbrella.",
+    "may": "Spring is in full bloom. Enjoy the fresh air and nature.",
+    "june": "Summer's here. Time for beach trips and BBQs.",
+    "july": "Watch the fireworks. Happy Independence Day!",
+    "august": "The hottest month. Stay hydrated and wear sunscreen.",
+    "september": "Fall is approaching. Get your sweaters out.",
+    "october": "Pumpkin spice everything. Prepare for Halloween.",
+    "november": "Leaves are falling. Time to give thanks.",
+    "december": "Holiday season. Spend time with loved ones and spread cheer.",
+}
 
 # Create your views here.
+
+
+def monthly_challenge(request, month):
+    try:
+        text = months[month]
+        return HttpResponse(text)
+    except:
+        return HttpResponseNotFound("Could not find that month.")
+
+
+def monthly_challenge_redirect(request, month):
+    try:
+        keys_of_months = list(months.keys())
+        redirect_month = keys_of_months[month - 1]
+        return HttpResponseRedirect("/challenges/" + redirect_month)
+    except:
+        return HttpResponseNotFound("Number needs to be between 1-12")
