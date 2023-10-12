f828174ceefdcedb5b69c82d5b804e5ba37303d1
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-10-12 23:40:44 +0200

    UpdateP

--- Django/myproject/myapp/urls.py
+++ Django/myproject/myapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<int:month>", views.month_by_number, name="month_by_number"),
+    path("<int:month>", views.month_by_number),
     path("<str:month>", views.month, name="month"),
 ]
--- Django/myproject/myapp/views.py
+++ Django/myproject/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 months = [
     "January",
@@ -19,13 +20,27 @@ months = [
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(
+        request,
+        "myapp/index.html",
+        {
+            "title": "Months",
+            "months": months,
+        },
+    )
 
 
 def month_by_number(request, month):
     month = months[(month - 1) % 12]
-    return HttpResponseRedirect(month)
+    redirect_path = reverse("month", args=[month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def month(request, month):
-    return HttpResponse(month)
+    return render(
+        request,
+        "myapp/month.html",
+        {
+            "month": month,
+        },
+    )
