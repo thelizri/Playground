c01a43ae7f4fbae03d9eafe0e93364dab65ae578
Karlan <wcar@kth.se>
2023-10-16 01:53:26 +0200

    Update

--- Django/blog/blog_posts/models.py
+++ Django/blog/blog_posts/models.py
@@ -2,9 +2,6 @@ from django.db import models
 
 
 class BlogPost(models.Model):
-    id = models.CharField(
-        max_length=255, unique=True
-    )  # unique=True ensures no two blog posts can have the same URL
     title = models.CharField(max_length=255)
     content = models.TextField()
     author = models.CharField(max_length=255)
