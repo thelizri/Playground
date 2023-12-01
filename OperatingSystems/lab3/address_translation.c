c26b832b85f125f2e3b38b82f200280a0a63cd20
Karlan <wcar@kth.se>
2023-12-01 18:11:20 +0100

    Can read from fake disk

--- OperatingSystems/lab3/address_translation.c
+++ OperatingSystems/lab3/address_translation.c
@@ -1,7 +1,9 @@
 #include <stdio.h>   // Standard input-output header for printf and file operations
 #include <stdlib.h>  // Standard library for exit function
 
-#define PATH_ADDRESSES "lab3_data/addresses.txt"
+#define PATH_ADDRESS "lab3_data/addresses.txt"
+#define PAGE_SIZE 256
+#define DISK_ADDRESS "lab3_data/BACKING_STORE.bin"
 
 typedef struct {
     int frame_number;
@@ -10,23 +12,41 @@ typedef struct {
 
 TLB_Unit TLB[15];
 
+//Reads from fake disk
+int read_disk(int page, char* buffer, FILE* file) {
+    // Seek to the correct page in the file
+    if (fseek(file, (long) page * PAGE_SIZE, SEEK_SET) != 0) {
+        return -1; // Return an error code if seek fails
+    }
+
+    // Read PAGE_SIZE bytes from the file into the buffer
+    size_t bytesRead = fread(buffer, 1, PAGE_SIZE, file);
+
+    // Check if the read operation was successful
+    if (bytesRead != PAGE_SIZE) {
+        return -1; // Return an error code if read fails
+    }
+
+    return 0; // Return success
+}
+
 // Main function
 int main() {
     FILE *file;  // File pointer
-    char line[256];  // Buffer to hold each line of text
+    char buffer[PAGE_SIZE];  // Buffer to hold each buffer of text
 
+    
     // Open the file for reading
-    file = fopen(PATH_ADDRESSES, "r");
+    file = fopen(PATH_ADDRESS, "r");
 
     // Check if file opening succeeded
     if (file == NULL) {
         perror("Error opening file");  // Print error message
         exit(EXIT_FAILURE);  // Exit program with failure status
     }
-
-    // Read and print each line of the file
-    while (fgets(line, sizeof(line), file)) {
-        int number = atoi(line); // Convert line to integer
+    // Read and print each buffer of the file
+    while (fgets(buffer, sizeof(buffer), file)) {
+        int number = atoi(buffer); // Convert buffer to integer
         int offset = number & 0xFF;
         int page_number = number >> 8;
         printf("Number: 0x%x\n", number); // Print the integer
@@ -36,9 +56,21 @@ int main() {
 
     // Close the file
     fclose(file);
+    
+
+    //Test reading binary
+    file = fopen(DISK_ADDRESS, "rb");
+    int return_value = read_disk(1, buffer, file);
+    printf("Return value: %d\n", return_value);
+    for(int i = 0; i < PAGE_SIZE; i++){
+        printf("0x%x\n", *(buffer+i));
+    }
+    fclose(file);
+
 
     return 0;
 }
+
 
 /*
 * 16 entries in the TLB
@@ -48,3 +80,5 @@ int main() {
 * Frame size of 256 bytes
 * Physical memory of 65 536 bytes (256x256)
 */
+
+/*Bypass TLB and use a only a page table in the beginning*/
