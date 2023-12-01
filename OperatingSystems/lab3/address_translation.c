0f48da028b203909e2a21f835844d22a3d281b6f
Karlan <wcar@kth.se>
2023-12-01 16:57:14 +0100

    Can extract page number and offset

--- OperatingSystems/lab3/address_translation.c
+++ OperatingSystems/lab3/address_translation.c
@@ -3,6 +3,13 @@
 
 #define PATH_ADDRESSES "lab3_data/addresses.txt"
 
+typedef struct {
+    int frame_number;
+    int valid_bit;
+} TLB_Unit;
+
+TLB_Unit TLB[15];
+
 // Main function
 int main() {
     FILE *file;  // File pointer
@@ -19,7 +26,12 @@ int main() {
 
     // Read and print each line of the file
     while (fgets(line, sizeof(line), file)) {
-        printf("%s", line);
+        int number = atoi(line); // Convert line to integer
+        int offset = number & 0xFF;
+        int page_number = number >> 8;
+        printf("Number: 0x%x\n", number); // Print the integer
+        printf("Page Number: 0x%x\n", page_number);
+        printf("Offset: 0x%x\n", offset);
     }
 
     // Close the file
@@ -27,3 +39,12 @@ int main() {
 
     return 0;
 }
+
+/*
+* 16 entries in the TLB
+* 256 entries in the page table
+* Page size of 256 bytes
+* 256 frames in the physical memory
+* Frame size of 256 bytes
+* Physical memory of 65 536 bytes (256x256)
+*/
